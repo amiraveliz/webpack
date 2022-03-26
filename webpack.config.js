d59cdb072da6be7084e90703b1f68cb16ff2e39b
Amira Veliz <amira.veliz@gmail.com>
2022-03-26 19:43:38 -0300

    initial configuration webpack with eslint, prettier lin-staged

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const ruleForStyles = {
+  test: /\.css$/,
+  use: ["style-loader", "css-loader"],
+};
+
+const ruleForJavascript = {
+  test: /\.js$/,
+  loader: "babel-loader",
+  options: {
+    presets: [
+      [
+        "@babel/preset-react",
+        {
+          runtime: "automatic",
+        },
+      ],
+    ],
+  },
+};
+
+const rules = [ruleForJavascript, ruleForStyles];
+
+module.exports = (env, argv) => {
+  const { mode } = argv;
+  const isProducttion = mode === "production";
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      filename: isProducttion ? "[name].[contenthash].js" : "main.js",
+      path: path.resolve(__dirname, "build"),
+    },
+    module: { rules },
+    plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+    devServer: {
+      open: true,
+      port: 3000,
+      compress: true,
+    },
+    devtool: "source-map",
+  };
+};
